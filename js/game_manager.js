e02ecac3675840a83a6a43f5651f65935e6ebea6
The123Qwerty <jafar.alngaar@gmail.com>
2025-11-19 00:26:40 +0100

    Try again

--- js/game_manager.js
+++ js/game_manager.js
@@ -64,10 +64,10 @@ GameManager.prototype.addRandomTile = function () {
     var text = value.toString();
     if (value > 99990)
     {
-      text = "First if and value % 10 is ${value % 10}";
+      text = `First if and value % 10 is ${value % 10}`;
       // text = alphabet[value % 10];
       if (value > 999900)
-        text = "Second if and value % 100 is ${value % 100}";
+        text = `Second if and value % 100 is ${value % 100}`;
         // text = alphabet[value % 100];
     }
     var tile = new Tile(this.grid.randomAvailableCell(), value, text);
