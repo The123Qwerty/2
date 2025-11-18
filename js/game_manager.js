acf10c830871db2f4c968753f3d27068d9ae055e
The123Qwerty <jafar.alngaar@gmail.com>
2025-11-18 23:44:16 +0100

    Value to letter conversion added

--- js/game_manager.js
+++ js/game_manager.js
@@ -58,9 +58,15 @@ GameManager.prototype.addStartTiles = function () {
 
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
+  const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   if (this.grid.cellsAvailable()) {
     var value = Math.random() < 0.99 ? 99991 : 1;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
+    tile.text = value;
+    if (value > 99990)
+    {
+      tile.text = alphabet[value % 10];
+    }
     
     this.grid.insertTile(tile);
     this.score += 1;
