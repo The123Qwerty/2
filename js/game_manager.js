4624b64ff2fcbbfdef2809905e6cb98e1b1ff419
The123Qwerty <jafar.alngaar@gmail.com>
2025-11-19 01:53:22 +0100

    Is it even updating

--- js/game_manager.js
+++ js/game_manager.js
@@ -58,7 +58,7 @@ GameManager.prototype.addStartTiles = function () {
 
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
-  const alphabet = [" ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  const alphabet = [" ", "A", "B...", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   if (this.grid.cellsAvailable()) {
     var value = Math.random() < 0.9999 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? 999926: 999925: 999924: 999923: 999922: 999921: 999920: 999919: 999918: 999917: 999916: 999915: 999914: 999913: 999912: 999911: 999910: 99999: 99998: 99997: 99996: 99995: 99994: 99993: 99992: 99991: 1;
     var text = value.toString();
@@ -68,7 +68,7 @@ GameManager.prototype.addRandomTile = function () {
 
       for (let i = 0; i < 3; i++) {
           var tempText = Math.random() < 0.333 ? Math.random() < 0.25 ? Math.random() < 0.2 ? Math.random() < 0.166667 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? Math.random() < 0.1 ? "Z": "Y": "X": "W": "V": "U": "T": "S": "R": "Q": "P": "O": "N": "M": "L": "K": "J": "I": "H": "G": "F": "E": "D": "C": "B": "A": ".";
-          if (tempText = "") {break;}
+          //if (tempText = "") {break;}
           text += tempText;
         }
     }
