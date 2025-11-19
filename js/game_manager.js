07c557f6ae4875d8be7823486ea3d355e950dbf6
The123Qwerty <jafar.alngaar@gmail.com>
2025-11-19 17:52:32 +0100

    Check values with console.log

--- js/game_manager.js
+++ js/game_manager.js
@@ -150,7 +150,8 @@ GameManager.prototype.move = function (direction) {
         // Only one merger per row traversal?
         if (next && next.text === tile.text && !next.mergedFrom) { 
           var merged = new Tile(positions.next, tile.value, tile.text, Math.min(next.id, tile.id));
-          //this.ids[Math.max(next.id, tile.id) - 1] = false;
+          console.log("next.id: " + next.id + ", tile.id: " + tile.id + "ids: " + ids.toString());
+          this.ids[Math.max(next.id, tile.id) - 1] = false;
           merged.mergedFrom = [tile, next];
 
           self.grid.insertTile(merged);
