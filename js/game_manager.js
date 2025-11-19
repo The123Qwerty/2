cbc6f77b7bad0c221909777fcfd3f5f73b2c8179
The123Qwerty <jafar.alngaar@gmail.com>
2025-11-19 17:55:41 +0100

    Forgot 'this.'

--- js/game_manager.js
+++ js/game_manager.js
@@ -150,7 +150,7 @@ GameManager.prototype.move = function (direction) {
         // Only one merger per row traversal?
         if (next && next.text === tile.text && !next.mergedFrom) { 
           var merged = new Tile(positions.next, tile.value, tile.text, Math.min(next.id, tile.id));
-          console.log("next.id: " + next.id + ", tile.id: " + tile.id + "ids: " + ids.toString());
+          console.log("next.id: " + next.id + ", tile.id: " + tile.id + "ids: " + this.ids.toString());
           this.ids[Math.max(next.id, tile.id) - 1] = false;
           merged.mergedFrom = [tile, next];
 
