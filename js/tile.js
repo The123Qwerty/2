9cce7b177420cd755fd45c9284c3fe802b78e571
The123Qwerty <jafar.alngaar@gmail.com>
2025-11-19 16:24:52 +0100

    Added id to tile

--- js/tile.js
+++ js/tile.js
@@ -1,8 +1,9 @@
-function Tile(position, value, text = value) {
+function Tile(position, value, text, id = -1) {
   this.x                = position.x;
   this.y                = position.y;
   this.value            = value;
   this.text             = text;
+  this.id               = id;
 
   this.previousPosition = null;
   this.mergedFrom       = null; // Tracks tiles that merged together
