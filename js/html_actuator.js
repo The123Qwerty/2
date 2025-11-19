4511dc3040dac5b54d3b213817307f3d4bdfc007
The123Qwerty <jafar.alngaar@gmail.com>
2025-11-19 16:57:37 +0100

    Scary change, can define style per individual tile now?

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -60,7 +60,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   var positionClass = this.positionClass(position);
 
   // We can't use classlist because it somehow glitches when replacing classes
-  var classes = ["tile", "tile-" + tile.value, positionClass];
+  var classes = ["tile", "tile-" + tile.id, positionClass];
 
   if (tile.value > 999999999999999999999999999999999999999999999) classes.push("tile-super");
 
