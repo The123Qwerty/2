f2c2a6fc9f42a6694b11832980f0d6ded388281b
The123Qwerty <jafar.alngaar@gmail.com>
2025-11-21 04:54:00 +0100

    Fixed another simple mistake

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -51,7 +51,7 @@ HTMLActuator.prototype.clearContainer = function (container) {
   }
 };
 
-HTMLActuator.prototype.setTileColor = function (id, tileText, colorNum, inner) {
+HTMLActuator.prototype.setTileColor = function (id, tileText, inner) {
   const tileColors = [
     ["1",  "7c7f66"],
     ["A", "#725e6b"],
