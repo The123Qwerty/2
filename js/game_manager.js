6501358b85dcde6ad63ce76995387892e3ef05ba
The123Qwerty <jafar.alngaar@gmail.com>
2025-11-21 22:45:55 +0100

    Don't move tiles if animation is running

--- js/game_manager.js
+++ js/game_manager.js
@@ -129,7 +129,7 @@ GameManager.prototype.move = function (direction) {
   // 0: up, 1: right, 2:down, 3: left
   var self = this;
 
-  if (this.isGameTerminated()) return; // Don't do anything if the game's over
+  if (this.isGameTerminated() || actuator.animationRunning) return; // Don't do anything if the game's over or an animation is running
 
   var cell, tile;
 
