e34a732eadc82c4449f0490fd7f6a80e399c9ae6
The123Qwerty <jafar.alngaar@gmail.com>
2025-11-23 17:42:27 +0100

    Made it so the 3 wins, instead of the 2

--- js/game_manager.js
+++ js/game_manager.js
@@ -99,7 +99,7 @@ GameManager.prototype.randomTile = function (list) {
     {
       if (!(Math.random() < 1 - (1 / list[i][1])))
       {
-        if (list[i][0] === "2") this.won = true;
+        if (list[i][0] === "3") this.won = true;
         return list[i][0];
       }
     }
