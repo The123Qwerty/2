b0f4ffcc797efaa712f39f321836a5c3adad7757
The123Qwerty <jafar.alngaar@gmail.com>
2026-02-04 15:26:20 +0100

    Fixed mistake

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -236,7 +236,7 @@ HTMLActuator.prototype.newTilePopup = function(inner, text)
   rarityBox.style.color = '#f9f6f2';
   rarityBox.style.padding = '10px 10px';
   rarityBox.textContent = `New tile discovered:
-  1 in " + this.addCommas(this.getTotalRarity(text))`;
+  1 in ` + this.addCommas(this.getTotalRarity(text));
   rarityBox.style.display = "inline-block";
   rarityBox.style.verticalAlign = "middle";
   rarityBox.style.opacity = '1';
